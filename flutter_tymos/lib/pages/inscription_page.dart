b4666a1ffcff2ab1917a07916d92fd68dd3819ea
ThbMrv <thibaud.morvan18@gmail.com>
2024-11-13 11:39:13 +0100

    Padding page inscription

--- flutter_tymos/lib/pages/inscription_page.dart
+++ flutter_tymos/lib/pages/inscription_page.dart
@@ -12,7 +12,8 @@ class InscriptionPage extends StatelessWidget {
           Container(
             color: Colors.white,
           ),
-          Align( // Logo
+          Align(
+            // Logo
             alignment: Alignment.topCenter,
             child: Padding(
               padding: const EdgeInsets.only(top: 120),
@@ -23,25 +24,25 @@ class InscriptionPage extends StatelessWidget {
             ),
           ),
           Align(
-            child: Padding(
+              child: Padding(
             padding: EdgeInsets.only(bottom: 480),
-              child: Text(
-                'revisitez chez vous !',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18, 
-                  color: Colors.black, 
-                  fontWeight: FontWeight.normal, 
-                ),
+            child: Text(
+              'revisitez chez vous !',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
               ),
-            )
-          ),
+            ),
+          )),
           Container(
             width: 300,
             alignment: Alignment.center,
             child: Padding(
               padding: const EdgeInsets.only(bottom: 200),
               child: Container(
+                height: 40, // Fixe la hauteur pour contrôler l'espace vertical
                 decoration: BoxDecoration(
                   color: Colors.white, // Fond blanc
                   borderRadius: BorderRadius.circular(30),
@@ -50,15 +51,21 @@ class InscriptionPage extends StatelessWidget {
                     width: 1,
                   ),
                 ),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'arthurobraqueto@gmail.com', // Texte indicatif
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.symmetric(vertical: 5), // Espace interne
-                    hintStyle: TextStyle(color : Colors.grey) // Pas de bordure interne
+                child: Center(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        hintText: 'thibaud@gmail.com', // Texte indicatif
+                        border: InputBorder.none,
+                        isDense: true, // Réduit légèrement l'espace interne
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0), // Espace interne
+                        hintStyle: TextStyle(
+                            color: Colors.grey) // Pas de bordure interne
+                        ),
+                    style: TextStyle(
+                        fontSize: 13, color: Colors.black), // Texte noir
                   ),
-                  style: TextStyle(fontSize: 13, color: Colors.black), // Texte noir
                 ),
               ),
             ),
@@ -69,6 +76,7 @@ class InscriptionPage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.only(bottom: 100),
               child: Container(
+                height: 40, // Fixe la hauteur pour contrôler l'espace vertical
                 decoration: BoxDecoration(
                   color: Colors.white, // Fond blanc
                   borderRadius: BorderRadius.circular(30),
@@ -77,76 +85,80 @@ class InscriptionPage extends StatelessWidget {
                     width: 1,
                   ),
                 ),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'arthurobraqueto@gmail.com', // Texte indicatif
-                    border: InputBorder.none,
-                    hintStyle: TextStyle(color : Colors.grey) // Pas de bordure interne
+                child: Center(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        hintText: 'patrock@gmail.com', // Texte indicatif
+                        border: InputBorder.none,
+                        isDense: true, // Réduit légèrement l'espace interne
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0), // Espace interne
+                        hintStyle: TextStyle(
+                            color: Colors.grey) // Pas de bordure interne
+                        ),
+                    style: TextStyle(
+                        fontSize: 13, color: Colors.black), // Texte noir
                   ),
-                  style: TextStyle(fontSize: 13, color: Colors.black), // Texte noir
                 ),
               ),
             ),
           ),
           Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: GestureDetector(
-                onTap: () {
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color(0xFF84CFEE), 
-                        Color(0xFF9BCB8E), 
-                        Color(0xFFFAEA78),
-                      ],
-                    ),
-                    borderRadius: BorderRadius.circular(30), 
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 10), 
-                  child: const Text(
-                    'Continuer',
-                    style: TextStyle(
-                      fontSize: 15, 
-                      color: Colors.white, 
-                    ),
-                  ),
-                )
-              )
-            )
-          ),
-            Align( // Texte de connexion
+              alignment: Alignment.center,
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [
+                              Color(0xFF84CFEE),
+                              Color(0xFF9BCB8E),
+                              Color(0xFFFAEA78),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 120, vertical: 10),
+                        child: const Text(
+                          'Continuer',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )))),
+          Align(
+            // Texte de connexion
             alignment: Alignment.bottomCenter,
             child: Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/connexion');
-                },
-                child: RichText(
-                  text: const TextSpan(
-                    text: "Vous avez déjà un compte ? ",
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black, 
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "Connectez-vous", 
-                        style: TextStyle(
-                          color: Color(0xFF84CFEE),
-                          fontWeight: FontWeight.bold, 
-                        ),
+                padding: const EdgeInsets.only(bottom: 50),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/connexion');
+                  },
+                  child: RichText(
+                    text: const TextSpan(
+                      text: "Vous avez déjà un compte ? ",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
                       ),
-                    ],
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Connectez-vous",
+                          style: TextStyle(
+                            color: Color(0xFF84CFEE),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              )
-            ),
+                )),
           ),
         ],
       ),
